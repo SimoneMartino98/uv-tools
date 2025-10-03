8984732f5880d9a12b6d70201356fa0343b99aee
Simone Martino <s.martino0898@gmail.com>
2025-10-03 12:31:44 +0200

    comments.

--- src/uv_tool/main.py
+++ src/uv_tool/main.py
@@ -9,11 +9,13 @@ def main():
     parser = argparse.ArgumentParser(prog="uv-tool", description="Manage global envs with uv")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
+    # uv-tool create <name> [--python <version>]
     create_parser = subparsers.add_parser("create", help="Create a new global environment")
     create_parser.add_argument("name", help="Name of the environment")
     create_parser.add_argument("--python", help="Python version to be installed", required=False)
     create_parser.set_defaults(func=cmd_create)
 
+    # uv-tool remove <name>
     remove_parser = subparsers.add_parser("remove", help="remove a specific global environment")
     remove_parser.add_argument("name", help="Name of the environment")
     remove_parser.set_defaults(func=cmd_remove)
